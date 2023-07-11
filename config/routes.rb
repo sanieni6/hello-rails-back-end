67d7aeb798da617664f0cb7b8c637aecfbf0fd4a
sanieni6 <luissanchezzamora23@gmail.com>
2023-07-11 11:25:26 -0500

    Create the greetiong API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :api do
-    namespace :v1 do
       resources :greetings, only: [:index]
-    end
-  end
 end
